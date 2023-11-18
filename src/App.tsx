d54bf040514582cc204d3dd7ba003d3fc2fb3092
João Carlos <joaocdfariass@gmail.com>
2023-11-18 20:55:39 -0300

    refactor: remove useAxios hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
-import { useAxios } from './hooks'
-
-interface IData {
-  userId: number
-  id: number
-  title: string
-  completed: boolean
-}
-
 function App() {
-  const { data, isLoading } = useAxios<IData[]>({
-    url: 'https://jsonplaceholder.typicode.com/todos/',
-    method: 'GET',
-  })
-
   return (
     <>
       <h1>Hello World</h1>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
-          {data?.map((item) => {
-            return <li key={item.id}> {item.title} </li>
-          })}
-        </ul>
-      )}
     </>
   )
 }
--- src/hooks/tests/use-axios.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest'
-
-describe('useAxios', () => {
-  it.todo('should return data correctly', async () => {})
-  it.todo('should return error', async () => {})
-  it.todo('should return isLoading', async () => {})
-  it.todo('should call refetch', async () => {})
-})
