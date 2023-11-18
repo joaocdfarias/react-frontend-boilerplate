a958ce8a770df6cec7a3b1786b604afacdd29bda
João Carlos <joaocdfariass@gmail.com>
2023-11-18 15:40:07 -0300

    feat: add example for get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { useAxios } from './hooks'
+
+interface IData {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
+
 function App() {
+  const { data, isLoading } = useAxios<IData[]>({
+    url: 'https://jsonplaceholder.typicode.com/todos/',
+    method: 'GET',
+  })
+
   return (
     <>
       <h1>Hello World</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data?.map((item) => {
+            return <li key={item.id}> {item.title} </li>
+          })}
+        </ul>
+      )}
     </>
   )
 }
