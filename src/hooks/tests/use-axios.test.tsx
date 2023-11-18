b143df48ae4a07d216dd788c0f5bf976b91f1c01
João Carlos <joaocdfariass@gmail.com>
2023-11-18 15:40:34 -0300

    test: create axios hook

---
+++ src/hooks/tests/use-axios.test.tsx
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest'
+
+describe('useAxios', () => {
+  it.todo('should return data correctly', async () => {})
+  it.todo('should return error', async () => {})
+  it.todo('should return isLoading', async () => {})
+  it.todo('should call refetch', async () => {})
+})
